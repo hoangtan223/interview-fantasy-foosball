8af60308039cea12a280a9e03564ce0439a40cfd
Tân Bùi <hoangtan223@gmail.com>
2017-09-01 23:20:17 +0700

    Add seed factory for user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if User.count == 0
+  10.times do
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    User.create!(first_name: first_name, last_name: last_name, email: Faker::Internet.free_email("#{first_name}_#{last_name}"),
+                 password: '123123', password_confirmation: '123123')
+  end
+end
