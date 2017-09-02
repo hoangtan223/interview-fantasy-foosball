a73f6d5fbfafafa998aa066f4893c8ccc89518f0
Tân Bùi <hoangtan223@gmail.com>
2017-09-02 15:51:06 +0700

    Add logic to validate not allow new team member when team already has 2 members

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,6 +1,13 @@
 class TeamMember < ApplicationRecord
   belongs_to :user
   belongs_to :team
+  validate :team_is_not_full
 
   enum status: {team_owner: 0, invited: 1, accepted: 2}
+
+  def team_is_not_full
+    if TeamMember.where(team_id: self.team_id).count == 2
+      errors.add(:team, "is full")
+    end
+  end
 end
--- spec/models/team_member_spec.rb
+++ spec/models/team_member_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe TeamMember, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:team) }
+
+  describe "create new team member" do
+    let(:users) { create_list(:user, 5) }
+    let(:team) { create(:team) }
+    let(:full_team) do
+      team.team_members.create(user: users.first, status: 'team_owner')
+      team.team_members.create(user: users.last, status: 'accepted')
+      team
+    end
+
+    it 'should validate when team is full' do
+      new_user = create(:user)
+      team_member = full_team.team_members.build(user: new_user)
+      expect(team_member.save).to be false
+      expect(team_member.errors.full_messages).to include("Team is full")
+    end
+
+  end
 end
